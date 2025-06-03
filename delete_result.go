02e88a470a1c34c0d4606b068f0b1a5d75a95cd4
jfg4 <jfg@tangentsolutionsinc.com>
2025-06-03 13:11:15 +0800

    Optimize new interface implementation (delete_result.go)

---
+++ delete_result.go
@@ -0,0 +1,38 @@
+// Random Go struct generated on 2025-06-03
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type ProcessItem struct {
+    Name      string    `json:"name"`
+    Value     int       `json:"value"`
+    Active    bool      `json:"active"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+func NewProcessItem(name string, value int) *ProcessItem {
+    return &ProcessItem{
+        Name:      name,
+        Value:     value,
+        Active:    true,
+        CreatedAt: time.Now(),
+    }
+}
+
+func (s *ProcessItem) UpdateValue(newValue int) {
+    s.Value = newValue
+}
+
+func (s *ProcessItem) ToJSON() ([]byte, error) {
+    return json.Marshal(s)
+}
+
+func main() {
+    obj := NewProcessItem("sample", 59)
+    data, _ := obj.ToJSON()
+    fmt.Println(string(data))
+}
