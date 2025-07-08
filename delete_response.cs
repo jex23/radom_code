f6f6db14c2d17ee1ef6a61dce1144ba31c6cce2d
jex23 <jamesgalos223@gmail.com>
2025-07-08 08:01:00 +0800

    Refactor LINQ extension in delete_response.cs

---
+++ delete_response.cs
@@ -0,0 +1,71 @@
+// Random C# class generated on 2025-07-08
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+public class UpdateResponse : INotifyPropertyChanged
+{
+    private string _name;
+    private int _value;
+    private readonly List<string> _history;
+    
+    public event PropertyChangedEventHandler PropertyChanged;
+    
+    public UpdateResponse(string name, int value = 49)
+    {
+        _name = name;
+        _value = value;
+        _history = new List<string>();
+        CreatedAt = DateTime.Now;
+        AddToHistory($"Created with name: {name}, value: {value}");
+    }
+    
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != value)
+            {
+                var oldValue = _name;
+                _name = value;
+                AddToHistory($"Name changed from {oldValue} to {value}");
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+    }
+    
+    public int Value
+    {
+        get => _value;
+        set
+        {
+            if (_value != value)
+            {
+                var oldValue = _value;
+                _value = value;
+                AddToHistory($"Value changed from {oldValue} to {value}");
+                OnPropertyChanged(nameof(Value));
+            }
+        }
+    }
+    
+    public DateTime CreatedAt { get; }
+    
+    public IReadOnlyList<string> History => _history.AsReadOnly();
+    
+    private void AddToHistory(string entry)
+    {
+        _history.Add($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {entry}");
+    }
+    
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    
+    public override string ToString()
+    {
+        return $"UpdateResponse { Name = {Name}, Value = {Value}, CreatedAt = {CreatedAt} }";
+    }
+}
