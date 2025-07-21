16802d1af626a6cf30742646d51d510251c200a2
jex23 <jamesgalos223@gmail.com>
2025-07-21 10:23:01 +0800

    Build new async handler (check_value.dart)

---
+++ check_value.dart
@@ -0,0 +1,26 @@
+// Random Dart class generated on 2025-07-21
+
+class ProcessItem {
+  String _name;
+  int _value;
+
+  ProcessItem(this._name, this._value);
+
+  String get name => _name;
+  int get value => _value;
+
+  set name(String newName) {
+    _name = newName;
+  }
+
+  set value(int newValue) {
+    _value = newValue;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _name,
+      'value': _value,
+    };
+  }
+}
