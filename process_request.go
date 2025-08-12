636ea7b3a71b75a2c36db7b573fea588250d9f4b
jex23 <jamesgalos223@gmail.com>
2025-08-12 11:21:01 +0800

    Refactor new package utility (process_request.go)

---
+++ process_request.go
@@ -0,0 +1,38 @@
+// Random Go struct generated on 2025-08-12
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type ValidateUser struct {
+    Name      string    `json:"name"`
+    Value     int       `json:"value"`
+    Active    bool      `json:"active"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+func NewValidateUser(name string, value int) *ValidateUser {
+    return &ValidateUser{
+        Name:      name,
+        Value:     value,
+        Active:    true,
+        CreatedAt: time.Now(),
+    }
+}
+
+func (s *ValidateUser) UpdateValue(newValue int) {
+    s.Value = newValue
+}
+
+func (s *ValidateUser) ToJSON() ([]byte, error) {
+    return json.Marshal(s)
+}
+
+func main() {
+    obj := NewValidateUser("sample", 85)
+    data, _ := obj.ToJSON()
+    fmt.Println(string(data))
+}
