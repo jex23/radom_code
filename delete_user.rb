48ecf4785debb814a79b37cecd3985358fdf7368
jex23 <jamesgalos223@gmail.com>
2025-08-14 10:02:02 +0800

    Update API wrapper - delete_user.rb

---
+++ delete_user.rb
@@ -0,0 +1,30 @@
+# Random Ruby class generated on 2025-08-14
+
+class DeleteValue
+  attr_reader :name, :value, :created_at
+  attr_writer :name, :value
+
+  def initialize(name, value = 32)
+    @name = name
+    @value = value
+    @created_at = Time.now
+  end
+
+  def to_hash
+    {
+      name: @name,
+      value: @value,
+      created_at: @created_at.iso8601
+    }
+  end
+
+  def update_value(new_value)
+    old_value = @value
+    @value = new_value
+    "Updated from #{old_value} to #{new_value}"
+  end
+end
+
+# Example usage
+obj = DeleteValue.new("sample", 18)
+puts obj.to_hash
