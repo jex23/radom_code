127044ab1f909a7bdbbff81960fccf4b867b2af4
jex23 <jamesgalos223@gmail.com>
2025-08-17 08:01:00 +0800

    Refactor optimized struct definition

---
+++ check_config.go
@@ -0,0 +1,38 @@
+// Random Go struct generated on 2025-08-17
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type CheckData struct {
+    Name      string    `json:"name"`
+    Value     int       `json:"value"`
+    Active    bool      `json:"active"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+func NewCheckData(name string, value int) *CheckData {
+    return &CheckData{
+        Name:      name,
+        Value:     value,
+        Active:    true,
+        CreatedAt: time.Now(),
+    }
+}
+
+func (s *CheckData) UpdateValue(newValue int) {
+    s.Value = newValue
+}
+
+func (s *CheckData) ToJSON() ([]byte, error) {
+    return json.Marshal(s)
+}
+
+func main() {
+    obj := NewCheckData("sample", 73)
+    data, _ := obj.ToJSON()
+    fmt.Println(string(data))
+}
