9bd38cf964ec2e464a8bbf31328218e811563f50
jex23 <jamesgalos223@gmail.com>
2025-09-01 13:13:06 +0800

    Add concurrent processor in process_config.go

---
+++ process_config.go
@@ -0,0 +1,38 @@
+// Random Go struct generated on 2025-09-01
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type DeleteValue struct {
+    Name      string    `json:"name"`
+    Value     int       `json:"value"`
+    Active    bool      `json:"active"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+func NewDeleteValue(name string, value int) *DeleteValue {
+    return &DeleteValue{
+        Name:      name,
+        Value:     value,
+        Active:    true,
+        CreatedAt: time.Now(),
+    }
+}
+
+func (s *DeleteValue) UpdateValue(newValue int) {
+    s.Value = newValue
+}
+
+func (s *DeleteValue) ToJSON() ([]byte, error) {
+    return json.Marshal(s)
+}
+
+func main() {
+    obj := NewDeleteValue("sample", 98)
+    data, _ := obj.ToJSON()
+    fmt.Println(string(data))
+}
