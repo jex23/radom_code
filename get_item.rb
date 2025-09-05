2631d44ea93120354cc0983285a2911c97b59c09
jex23 <jamesgalos223@gmail.com>
2025-09-05 13:11:03 +0800

    Refactor DSL builder in get_item.rb

---
+++ get_item.rb
@@ -0,0 +1,30 @@
+# Random Ruby class generated on 2025-09-05
+
+class CheckResponse
+  attr_reader :name, :value, :created_at
+  attr_writer :name, :value
+
+  def initialize(name, value = 27)
+    @name = name
+    @value = value
+    @created_at = Time.now
+  end
+
+  def to_hash
+    {
+      name: @name,
+      value: @value,
+      created_at: @created_at.iso8601
+    }
+  end
+
+  def update_value(new_value)
+    old_value = @value
+    @value = new_value
+    "Updated from #{old_value} to #{new_value}"
+  end
+end
+
+# Example usage
+obj = CheckResponse.new("sample", 50)
+puts obj.to_hash
