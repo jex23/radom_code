f04abd137a5951d89f9dc14fc3f07539b9e726e2
jex23 <jamesgalos223@gmail.com>
2025-09-10 13:55:11 +0800

    Build template class in get_request.cpp

---
+++ get_request.cpp
@@ -0,0 +1,66 @@
+// Random C++ header generated on 2025-09-10
+#ifndef CHECKVALUE_H
+#define CHECKVALUE_H
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace utils {
+
+template<typename T>
+class CheckValue {
+private:
+    std::vector<T> data_;
+    std::string name_;
+
+public:
+    explicit CheckValue(const std::string& name);
+    ~CheckValue() = default;
+    
+    void add(const T& item);
+    void remove(size_t index);
+    T& get(size_t index);
+    const T& get(size_t index) const;
+    
+    size_t size() const { return data_.size(); }
+    bool empty() const { return data_.empty(); }
+    
+    void clear();
+    std::string getName() const { return name_; }
+};
+
+// Template implementation
+template<typename T>
+CheckValue<T>::CheckValue(const std::string& name) : name_(name) {}
+
+template<typename T>
+void CheckValue<T>::add(const T& item) {
+    data_.push_back(item);
+}
+
+template<typename T>
+void CheckValue<T>::remove(size_t index) {
+    if (index < data_.size()) {
+        data_.erase(data_.begin() + index);
+    }
+}
+
+template<typename T>
+T& CheckValue<T>::get(size_t index) {
+    return data_.at(index);
+}
+
+template<typename T>
+const T& CheckValue<T>::get(size_t index) const {
+    return data_.at(index);
+}
+
+template<typename T>
+void CheckValue<T>::clear() {
+    data_.clear();
+}
+
+} // namespace utils
+
+#endif // CHECKVALUE_H
