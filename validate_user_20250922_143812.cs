86ffe6567feb6937c1b2ba6f8bfa584c6e11da6d
jex23 <jamesgalos223@gmail.com>
2025-09-22 14:38:12 +0800

    Enhance new .NET service (validate_user_20250922_143812.cs)

---
+++ validate_user_20250922_143812.cs
@@ -0,0 +1,48 @@
+// Random C# function generated on 2025-09-22
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GetValue
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Value { get; set; }
+    public DateTime CreatedAt { get; set; }
+    
+    public GetValue(int id, string name, double value)
+    {
+        Id = id;
+        Name = name;
+        Value = value;
+        CreatedAt = DateTime.Now;
+    }
+}
+
+public static class Delete_dataProcessor
+{
+    public static List<GetValue> Delete_data(IEnumerable<GetValue> items)
+    {
+        const double multiplier = 5.0;
+        
+        return items
+            .Where(item => item.Value > 0)
+            .Select(item => new GetValue(
+                item.Id,
+                $"{item.Name}_processed",
+                item.Value * multiplier))
+            .ToList();
+    }
+    
+    public static void Main()
+    {
+        var testData = new List<GetValue>
+        {
+            new GetValue(1, "Item1", 60),
+            new GetValue(2, "Item2", 72)
+        };
+        
+        var result = Delete_data(testData);
+        Console.WriteLine($"Processed {result.Count} items");
+    }
+}
