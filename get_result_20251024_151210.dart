918cca078ba46d3ec14ce7c0860243e9e952f4a9
jex23 <jamesgalos223@gmail.com>
2025-10-24 15:12:11 +0800

    Update optimized async handler

---
+++ get_result_20251024_151210.dart
@@ -0,0 +1,26 @@
+// Random Dart class generated on 2025-10-24
+
+class CheckResponse {
+  String _name;
+  int _value;
+
+  CheckResponse(this._name, this._value);
+
+  String get name => _name;
+  int get value => _value;
+
+  set name(String newName) {
+    _name = newName;
+  }
+
+  set value(int newValue) {
+    _value = newValue;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _name,
+      'value': _value,
+    };
+  }
+}
