1397db9ed66368a3ba5476980197147f785c3189
jex23 <jamesgalos223@gmail.com>
2025-10-30 19:22:15 +0800

    Add utility library in create_data_20251030_192215.c

---
+++ create_data_20251030_192215.c
@@ -0,0 +1,47 @@
+// Random C function generated on 2025-10-30
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char name[50];
+    double value;
+} Item;
+
+Item* update_data(const Item* items, int count, int* result_count) {
+    const double multiplier = 8.0;
+    Item* result = malloc(count * sizeof(Item));
+    
+    if (result == NULL) {
+        *result_count = 0;
+        return NULL;
+    }
+    
+    for (int i = 0; i < count; i++) {
+        result[i].id = items[i].id;
+        snprintf(result[i].name, sizeof(result[i].name), "%s_processed", items[i].name);
+        result[i].value = items[i].value * multiplier;
+    }
+    
+    *result_count = count;
+    return result;
+}
+
+int main() {
+    Item testData[] = {
+        {1, "item1", 35.0},
+        {2, "item2", 46.0}
+    };
+    
+    int result_count;
+    Item* result = update_data(testData, 2, &result_count);
+    
+    printf("Processed %d items\n", result_count);
+    
+    if (result) {
+        free(result);
+    }
+    
+    return 0;
+}
