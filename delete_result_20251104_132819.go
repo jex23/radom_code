6fe10dd25349ad25561423660aa823370f2cd8ae
jex23 <jamesgalos223@gmail.com>
2025-11-04 13:28:20 +0800

    Add optimized HTTP handler

---
+++ delete_result_20251104_132819.go
@@ -0,0 +1,38 @@
+// Random Go struct generated on 2025-11-04
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+    "time"
+)
+
+type UpdateConfig struct {
+    Name      string    `json:"name"`
+    Value     int       `json:"value"`
+    Active    bool      `json:"active"`
+    CreatedAt time.Time `json:"created_at"`
+}
+
+func NewUpdateConfig(name string, value int) *UpdateConfig {
+    return &UpdateConfig{
+        Name:      name,
+        Value:     value,
+        Active:    true,
+        CreatedAt: time.Now(),
+    }
+}
+
+func (s *UpdateConfig) UpdateValue(newValue int) {
+    s.Value = newValue
+}
+
+func (s *UpdateConfig) ToJSON() ([]byte, error) {
+    return json.Marshal(s)
+}
+
+func main() {
+    obj := NewUpdateConfig("sample", 67)
+    data, _ := obj.ToJSON()
+    fmt.Println(string(data))
+}
