8b541abe4d00d575e0bc62a7c4acd2009e3391eb
jex23 <jamesgalos223@gmail.com>
2025-11-26 06:07:21 +0800

    Create utility function with enhanced features

---
+++ validate_response_20251126_060721.dart
@@ -0,0 +1,27 @@
+// Random Flutter widget generated on 2025-11-26
+
+import 'package:flutter/material.dart';
+
+class HandleRequest extends StatelessWidget {
+  final String title;
+  final int value;
+
+  const HandleRequest({
+    Key? key,
+    required this.title,
+    this.value = 11,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Text(title, style: TextStyle(fontSize: 18)),
+          Text('Value: $value', style: TextStyle(fontSize: 14)),
+        ],
+      ),
+    );
+  }
+}
