5e3ca72b39b2f687460d037b65fe19c6d904844d
jex23 <jamesgalos223@gmail.com>
2025-12-17 21:22:18 +0800

    Add data processor with enhanced features

---
+++ check_config_20251217_212217.rb
@@ -0,0 +1,30 @@
+# Random Ruby class generated on 2025-12-17
+
+class CheckData
+  attr_reader :name, :value, :created_at
+  attr_writer :name, :value
+
+  def initialize(name, value = 76)
+    @name = name
+    @value = value
+    @created_at = Time.now
+  end
+
+  def to_hash
+    {
+      name: @name,
+      value: @value,
+      created_at: @created_at.iso8601
+    }
+  end
+
+  def update_value(new_value)
+    old_value = @value
+    @value = new_value
+    "Updated from #{old_value} to #{new_value}"
+  end
+end
+
+# Example usage
+obj = CheckData.new("sample", 11)
+puts obj.to_hash
