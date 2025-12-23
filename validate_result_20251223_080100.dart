9ab2e8204bec64aff909713551ef9356d020ecd9
jex23 <jamesgalos223@gmail.com>
2025-12-23 08:01:00 +0800

    Optimize Flutter widget

---
+++ validate_result_20251223_080100.dart
@@ -0,0 +1,34 @@
+// Random Dart function generated on 2025-12-23
+
+class GetConfig {
+  final int id;
+  final String name;
+  final double value;
+
+  const GetConfig({
+    required this.id,
+    required this.name,
+    required this.value,
+  });
+}
+
+List<GetConfig> update_result(List<GetConfig> items) {
+  const double multiplier = 8.0;
+  
+  return items
+      .map((item) => GetConfig(
+            id: item.id,
+            name: '${item.name}_processed',
+            value: item.value * multiplier,
+          ))
+      .toList();
+}
+
+void main() {
+  final testData = [
+    GetConfig(id: 1, name: 'Item1', value: 56.0),
+  ];
+
+  final result = update_result(testData);
+  print('Processed ${result.length} items');
+}
