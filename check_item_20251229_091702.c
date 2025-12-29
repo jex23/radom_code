9241c64b6d89700cf3da5e17abea9b97e58a0287
jex23 <jamesgalos223@gmail.com>
2025-12-29 09:17:02 +0800

    Implement new performance function (check_item_20251229_091702.c)

---
+++ check_item_20251229_091702.c
@@ -0,0 +1,47 @@
+// Random C function generated on 2025-12-29
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    int id;
+    char name[50];
+    double value;
+} Item;
+
+Item* validate_item(const Item* items, int count, int* result_count) {
+    const double multiplier = 3.0;
+    Item* result = malloc(count * sizeof(Item));
+    
+    if (result == NULL) {
+        *result_count = 0;
+        return NULL;
+    }
+    
+    for (int i = 0; i < count; i++) {
+        result[i].id = items[i].id;
+        snprintf(result[i].name, sizeof(result[i].name), "%s_processed", items[i].name);
+        result[i].value = items[i].value * multiplier;
+    }
+    
+    *result_count = count;
+    return result;
+}
+
+int main() {
+    Item testData[] = {
+        {1, "item1", 53.0},
+        {2, "item2", 58.0}
+    };
+    
+    int result_count;
+    Item* result = validate_item(testData, 2, &result_count);
+    
+    printf("Processed %d items\n", result_count);
+    
+    if (result) {
+        free(result);
+    }
+    
+    return 0;
+}
