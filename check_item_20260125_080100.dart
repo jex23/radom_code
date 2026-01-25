758cf1593cfffc8326aa93a9cebfe86631edad70
jex23 <jamesgalos223@gmail.com>
2026-01-25 08:01:01 +0800

    Build Flutter widget for improved functionality

---
+++ check_item_20260125_080100.dart
@@ -0,0 +1,26 @@
+// Random Dart class generated on 2026-01-25
+
+class CheckValue {
+  String _name;
+  int _value;
+
+  CheckValue(this._name, this._value);
+
+  String get name => _name;
+  int get value => _value;
+
+  set name(String newName) {
+    _name = newName;
+  }
+
+  set value(int newValue) {
+    _value = newValue;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _name,
+      'value': _value,
+    };
+  }
+}
