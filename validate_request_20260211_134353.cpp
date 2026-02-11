f7c29786ed1a24da396c1bef2da45f6e059d3954
jex23 <jamesgalos223@gmail.com>
2026-02-11 13:43:53 +0800

    Develop optimized template class

---
+++ validate_request_20260211_134353.cpp
@@ -0,0 +1,66 @@
+// Random C++ header generated on 2026-02-11
+#ifndef HANDLEDATA_H
+#define HANDLEDATA_H
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace utils {
+
+template<typename T>
+class HandleData {
+private:
+    std::vector<T> data_;
+    std::string name_;
+
+public:
+    explicit HandleData(const std::string& name);
+    ~HandleData() = default;
+    
+    void add(const T& item);
+    void remove(size_t index);
+    T& get(size_t index);
+    const T& get(size_t index) const;
+    
+    size_t size() const { return data_.size(); }
+    bool empty() const { return data_.empty(); }
+    
+    void clear();
+    std::string getName() const { return name_; }
+};
+
+// Template implementation
+template<typename T>
+HandleData<T>::HandleData(const std::string& name) : name_(name) {}
+
+template<typename T>
+void HandleData<T>::add(const T& item) {
+    data_.push_back(item);
+}
+
+template<typename T>
+void HandleData<T>::remove(size_t index) {
+    if (index < data_.size()) {
+        data_.erase(data_.begin() + index);
+    }
+}
+
+template<typename T>
+T& HandleData<T>::get(size_t index) {
+    return data_.at(index);
+}
+
+template<typename T>
+const T& HandleData<T>::get(size_t index) const {
+    return data_.at(index);
+}
+
+template<typename T>
+void HandleData<T>::clear() {
+    data_.clear();
+}
+
+} // namespace utils
+
+#endif // HANDLEDATA_H
