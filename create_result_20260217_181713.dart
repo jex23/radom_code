1c51e85278a27bb3af5f89a6086e1ababfc69e78
jex23 <jamesgalos223@gmail.com>
2026-02-17 18:17:13 +0800

    Implement state manager in create_result_20260217_181713.dart

---
+++ create_result_20260217_181713.dart
@@ -0,0 +1,26 @@
+// Random Dart class generated on 2026-02-17
+
+class ProcessUser {
+  String _name;
+  int _value;
+
+  ProcessUser(this._name, this._value);
+
+  String get name => _name;
+  int get value => _value;
+
+  set name(String newName) {
+    _name = newName;
+  }
+
+  set value(int newValue) {
+    _value = newValue;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': _name,
+      'value': _value,
+    };
+  }
+}
